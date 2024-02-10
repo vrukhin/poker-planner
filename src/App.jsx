076716d1d5843b43ed10a9e22b66b231accf6033
Pavel Vrukhin <vrukhin@protonmail.com>
2024-02-11 00:35:30 +0300

    Add vote component

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,56 @@ import './App.css'
 function App() {
 
   return (
+    <>
     <h1>Poker Planner</h1>
+    <Vote></Vote>
+    </>
   )
+}
+
+function Poker() {}
+
+function Vote({ taskKey }) {
+  taskKey = "KEY-XXX";
+  return(
+    <>
+    <h2>Vote for task: { taskKey }</h2>
+    <Voter></Voter>
+    <Voter></Voter>
+    <Voter></Voter>
+    <Voter></Voter>
+    <Voter></Voter>
+    <Voter></Voter>
+    <Voter></Voter>
+    <Voter></Voter>
+    </>
+  );
+}
+
+function Voter({ username }) {
+  username = "username";
+  return(
+    <div className='voter'>
+      <h3>{username}</h3>
+      <div className='vote-buttons'>
+        <VoteButton></VoteButton>
+        <VoteButton></VoteButton>
+        <VoteButton></VoteButton>
+        <VoteButton></VoteButton>
+        <VoteButton></VoteButton>
+        <VoteButton></VoteButton>
+        <VoteButton></VoteButton>
+        <VoteButton></VoteButton>
+      </div>
+    </div>
+  );
+}
+
+function VoteButton(){
+  // possible values: 1, 2, 3, 5, 8, 13, 20, 40
+  return (
+    <button className='vote-button'>{"0"}</button>
+  );
 }
 
 export default App
